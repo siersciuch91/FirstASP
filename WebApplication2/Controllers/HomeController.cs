6dc9053306b900bccae6f88ccfd751fbabfecd90
Adrian Grzegorek <siersciuch1991@gmail.com>
2018-02-24 11:10:48 +0100

    Initalize Commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      List<Kontakt> list = new List<Kontakt> { new Kontakt { imie = "Adrian", nazwisko = "Grzegorek", miasto= "Nielepice", ulica = "Sosnawa"},
+        new Kontakt{imie = "Kinga", nazwisko = "Kwiatkowska", miasto = "Charsznica", ulica = "Kolonia Chodów" } };
+
+      ViewBag.Kontakty = list;  
+
+      return View();
+    }
+
+    public ActionResult About()
+    {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+  }
+}
