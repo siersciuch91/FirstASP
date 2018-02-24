d081e912bca00ea1092d07cf2aafbc705af598d2
Adrian Grzegorek <siersciuch1991@gmail.com>
2018-02-24 11:25:34 +0100

    Refactor names

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -11,10 +11,21 @@ namespace WebApplication2.Controllers
   {
     public ActionResult Index()
     {
-      List<Kontakt> list = new List<Kontakt> { new Kontakt { imie = "Adrian", nazwisko = "Grzegorek", miasto= "Nielepice", ulica = "Sosnawa"},
-        new Kontakt{imie = "Kinga", nazwisko = "Kwiatkowska", miasto = "Charsznica", ulica = "Kolonia Chodów" } };
+      List<Contact> list = new List<Contact> { new Contact { firstName = "Adrian", secondName = "Grzegorek", city= "Nielepice", street = "Sosnawa"},
+        new Contact{firstName = "Kinga", secondName = "Kwiatkowska", city = "Charsznica", street = "Kolonia Chodów" } };
 
       ViewBag.Kontakty = list;  
+
+      return View();
+    }
+
+    public ActionResult Contacts()
+    {
+      List<Contact> list = new List<Contact> { new Contact { firstName = "Adrian", secondName = "Grzegorek", city= "Nielepice", street = "Sosnawa"},
+        new Contact{firstName = "Kinga", secondName = "Kwiatkowska", city = "Charsznica", street = "Kolonia Chodów" } };
+
+      ViewBag.Message = "Your contacts page.";
+      ViewBag.Contacts = list;
 
       return View();
     }
---
+++ WebApplication2/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+  public class Contact
+  {
+    public string firstName { get; set; }
+    public string secondName { get; set; }
+    public string city{ get; set; }
+    public string street { get; set; }
+  }
+}
